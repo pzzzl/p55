be46570d115fbab1b5a62c570453f6badad8a204
PZL <42525651+pzzzl@users.noreply.github.com>
2019-12-25 23:17:23 -0300

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,28 +1,37 @@
 import os
 from shutil import move
 
+#PATHS
 dir = os.getcwd()
 extracted = dir+'/extracted'
 
+#CHECKS IF FOLDER EXISTS AND CREATES IT IF DON'T
 try:
     os.chdir(extracted)
     os.chdir(dir)
 except:
     os.mkdir(extracted)
 
+#ASSIGN ARRAY <DIR NAME>, <LIST OF SUB DIRS>, <LIST OF FILES> INTO LIST
 list = list(os.walk(dir))
 
+#ASSIGN LIST SYZE TO ROOT AMOUNT AND PRINTS IT
 rootAmount = len(list)
-
 print('\nAnalyzing %s directories...' % rootAmount)
 
+#GENERATE FOLDER STRUCTURE
 for l in list:
     print('\n')
+
+    #VARIABLE
     nextDir = l[0]
     folders = l[1]
     files = l[2]
-    print(files)
 
+    #LOG FOLDERS WHILE PROCESSING
+    print(folders)
+
+    #CASE IT ISN'T 'EXTRACTED', RECEIVES FILE AND DESTINATION TO FINALLY MOVE IT
     if l == extracted:
         os.chdir(extracted)
         os.chdir(dir)
@@ -30,6 +39,7 @@ for l in list:
         for f in l[2]:
             file = nextDir+'/'+f
             destination = extracted+'/'+f
+
             try:
                 os.replace(file, destination)
             except PermissionError:
