6b19bfde90074a9612418996286924eb59c70c7f
Bruno Peselli <brunopeselli@gmail.com>
2021-07-15 19:40:19 -0300

    Refactoring

--- extract.py
+++ extract.py
@@ -1,49 +1,84 @@
 import os
+import sys
 from shutil import move
 
-#PATHS
-dir = os.getcwd()
-extracted = dir+'/extracted'
+arguments_len = len(sys.argv)
 
-#CHECKS IF FOLDER EXISTS AND CREATES IT IF DON'T
-try:
-    os.chdir(extracted)
-    os.chdir(dir)
-except:
-    os.mkdir(extracted)
+if(arguments_len < 2):
+    print('\n"PATH" needs to be specified.\nCommand structure: python extract.py "C:/path_to_the_extractable_folder"')
+    exit()
+elif(arguments_len > 2):
+    print('\nOnly ONE argument is required: "PATH"\nCommand structure: python extract.py "C:/path_to_the_extractable_folder"')
+    exit()
+elif(arguments_len == 2):
+    path = sys.argv[1]
+else:
+    print("\nIncorrect arguments structure.\nRead full documentation at https://github.com/pzzzl/py-extract")
+    exit()
 
-#ASSIGN ARRAY <DIR NAME>, <LIST OF SUB DIRS>, <LIST OF FILES> INTO LIST
-list = list(os.walk(dir))
+print("https://github.com/pzzzl/py-extract")
 
-#ASSIGN LIST SYZE TO ROOT AMOUNT AND PRINTS IT
-rootAmount = len(list)
-print('\nAnalyzing %s directories...' % rootAmount)
+if(not os.path.isdir(path)):
+    print("PATH doesn't exists")
+    exit()
 
-#GENERATE FOLDER STRUCTURE
-for l in list:
-    print('\n')
+print('\nAre you sure you want to extract all files from the folder "' + path + '" into a single folder "extracted"?')
+print("\n0. No\n1. Yes")
+answer = input("Option: ")
 
-    #VARIABLE
-    nextDir = l[0]
-    folders = l[1]
-    files = l[2]
+while(answer != "1" and answer != "0"):
+    print("The given option " + answer + " is invalid")
+    answer = input("Option: ")
 
-    #LOG FOLDERS WHILE PROCESSING
-    print(folders)
+if(answer == "0"):
+    print("\nFinishing script...")
+    exit()
 
-    #CASE IT ISN'T 'EXTRACTED', RECEIVES FILE AND DESTINATION TO FINALLY MOVE IT
-    if l == extracted:
-        os.chdir(extracted)
-        os.chdir(dir)
+extracted = path + '/extracted'
+
+if(not os.path.isdir(extracted)):
+    try:
+        print("Creating folder " + extracted)
+        os.mkdir(extracted)
+    except:
+        print("Error creating the specified folder. Finishing script...")
+        exit()
+else:
+    print("The folder " + extracted + " already exists")
+
+directories = list(os.walk(path))
+
+directories_len = len(directories)
+print('Analyzing %s directories...' % directories_len)
+
+# STATISTICS
+FILES_EXTRACTED = 0
+
+for folder_structure in directories:
+
+    current_directory = folder_structure[0]
+    folders = folder_structure[1]
+    files = folder_structure[2]
+
+    print("\nCurrent directory: " + str(current_directory))
+    print("Folders: " + str(folders))
+    print("Files: " + str(files))
+
+    if folder_structure == extracted:
+        pass
     else:
-        for f in l[2]:
-            file = nextDir+'/'+f
-            destination = extracted+'/'+f
-
+        for file_name in folder_structure[2]:
+            file_path = current_directory + '/' + file_name
+            destination = extracted + '/' + file_name
             try:
-                os.replace(file, destination)
+                print('Moving file "' + file_name + '"')
+                os.replace(file_path, destination)
+                FILES_EXTRACTED += 1
             except PermissionError:
-                print('\nFailed to move file "%s": being used by another process.\n' % f)
+                print('Failed to move file "%s": being used by another process\n' % file_name)
             except FileExistsError:
-                os.chdir(extracted)
-                os.chdir(dir)
+                print("File " + file_name + " already exists")
+                pass
+
+print("\nFILES EXTRACTED: " + str(FILES_EXTRACTED))
+print("FINISHED SUCCESFULLY")
