4f70dcbb9473ee9f3fbbd7987fc0c03c7d77c92a
Bruno Peselli <brunopeselli@gmail.com>
2021-07-19 11:55:32 -0300

    Removing unnecessary import

--- extract.py
+++ extract.py
@@ -1,9 +1,6 @@
-import os
-import sys
 import src.args as args
 import src.validation as validation
 import src.routine as routine
-from shutil import move
 
 if(__name__ == '__main__'):
     path = args.verify_path()
