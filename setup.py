607b4e92c23d4e037f5710e9b977b19b3b57cea8
Bruno Peselli <brunopeselli@gmail.com>
2021-07-21 17:32:51 -0300

    Installer

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(
-    # console=['p55.py'],
-    windows = [{'script': 'p55.py'}],
-    options={'py2exe': {'bundle_files': 1, 'compressed': True}},
-    zipfile = None
-    )
+setup(console=['p55.py'])
